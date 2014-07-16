413aaf46908056f1c7b0d29848cf41a935936a92
wangshijun2010 <wangshijun2010@gmail.com>
2014-07-16 20:59:56 +0800

    add gulp for building

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp    = require('gulp'),
+    path    = require('path'),
+    inject  = require('gulp-inject'),
+    stream  = require('event-stream');
+
+var plugins = require('gulp-load-plugins')();
+
+/**
+ * build Tasks
+ */
+gulp.task('build', function () {
+
+    // cleanup previous builds
+    gulp.src('dist/*.js', {read: false})
+        .pipe(plugins.clean());
+
+    // build js
+    var appJs = gulp.src('src/*.js')
+        .pipe(plugins.concat('angular-echarts.js'))
+        .pipe(plugins.removeUseStrict())
+        .pipe(plugins.uglify({ outSourceMap: true, mangle: true, report: 'gzip' }))
+        .pipe(plugins.size({ showFiles: true }))
+        .pipe(gulp.dest('dist'))
+
+    // bump bower, npm versions
+    gulp.src(['package.json', 'bower.json'])
+        .pipe(plugins.bump())
+        .pipe(gulp.dest('.'));
+
+});
+
