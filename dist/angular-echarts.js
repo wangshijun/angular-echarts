b3070f4f670bf3be6d294f7c30fa14a98bf49d20
Kerwin TianLang <kerwin612@qq.com>
2016-12-14 17:17:07 +0800

    support Provide 'chart-obj'(chartObj: a echarts instance) to users

--- dist/angular-echarts.js
+++ dist/angular-echarts.js
@@ -46,7 +46,7 @@ function getLinkFunction($http, theme, util, type) {
                     toolbox: angular.extend({ show: false }, angular.isObject(config.toolbox) ? config.toolbox : {}),
                     xAxis: [ angular.extend(xAxis, util.getAxisTicks(data, config, type)) ],
                     yAxis: [ yAxis ],
-                    graphic: config.graphic && (angular.isObject(config.graphic) || angular.isArray(config.graphic)) ? config.graphic : {},
+                    graphic: config.graphic && (angular.isObject(config.graphic) || angular.isArray(config.graphic)) ? config.graphic : [],
                     series: util.getSeries(data, config, type)
                 };
             if (!config.showXAxis) {
@@ -175,6 +175,7 @@ function getLinkFunction($http, theme, util, type) {
                     });
                 }
             }
+            scope.chartObj = chart;
         }
         // update when charts config changes
         scope.$watch(function () {
@@ -206,7 +207,8 @@ for (var i = 0, n = types.length; i < n; i++) {
                         template: '<div></div>',
                         scope: {
                             config: '=config',
-                            data: '=data'
+                            data: '=data',
+        					chartObj: '=chartObj'
                         },
                         link: getLinkFunction($http, theme, util, type)
                     };
--- src/directive.js
+++ src/directive.js
@@ -60,7 +60,7 @@ function getLinkFunction($http, theme, util, type) {
                 toolbox: angular.extend({ show: false }, angular.isObject(config.toolbox) ? config.toolbox : {}),
                 xAxis: [ angular.extend(xAxis, util.getAxisTicks(data, config, type)) ],
                 yAxis: [ yAxis ],
-				graphic: config.graphic && (angular.isObject(config.graphic) || angular.isArray(config.graphic)) ? config.graphic : {},
+				graphic: config.graphic && (angular.isObject(config.graphic) || angular.isArray(config.graphic)) ? config.graphic : [],
                 series: util.getSeries(data, config, type)
             };
 
@@ -190,6 +190,7 @@ function getLinkFunction($http, theme, util, type) {
                     chart.showLoading({ text: scope.config.errorMsg || '没有数据', textStyle: textStyle });
                 }
             }
+			scope.chartObj = chart;
         }
 
         // update when charts config changes
@@ -217,7 +218,8 @@ for (var i = 0, n = types.length; i < n; i++) {
                 template: '<div></div>',
                 scope: {
                     config: '=config',
-                    data: '=data'
+                    data: '=data',
+					chartObj: '=chartObj'
                 },
                 link: getLinkFunction($http, theme, util, type)
             };
